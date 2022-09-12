28bf155efcf26543f882065ec28c22adca782fc4
Amalrajanj <39754157+Amalrajanj@users.noreply.github.com>
2022-09-12 22:12:32 +0530

    test sample

--- Controllers/Start/Start.js
+++ Controllers/Start/Start.js
@@ -7,72 +7,70 @@ import { handlequery } from "./Handlequery.js";
 import { SubadminStart } from "./Subadmin.js";
 
 export const start = async (ctx) => {
-  // let query = ctx?.message?.text?.split(" ")[1];
+  let query = ctx?.message?.text?.split(" ")[1];
 
-  // let user = {
-  //   ...ctx.from,
-  //   admin: false,
-  //   banStatus: false,
-  // };
+  let user = {
+    ...ctx.from,
+    admin: false,
+    banStatus: false,
+  };
 
-  // await db.saveUser(user).then((res) => {
-  //   !res ? console.log("saveUser failed") : "";
-  // });
+  await db.saveUser(user).then((res) => {
+    !res ? console.log("saveUser failed") : "";
+  });
 
-  // await db.checkUser(ctx.from.id).then(async (res) => {
-  //   if (!res?.banStatus) {
-  //     let custom = await db.getcustom();
+  await db.checkUser(ctx.from.id).then(async (res) => {
+    if (!res?.banStatus) {
+      let custom = await db.getcustom();
 
-  //     await ctx.api
-  //       .getChatMember(
-  //         custom?.channelID || process.env.MAIN_CHANNEL,
-  //         ctx.from.id
-  //       )
-  //       .then(async (status) => {
-  //         if (status.status != "left") {
-  //           if (query) {
-  //             handlequery(ctx);
-  //           } else if (res) {
-  //             if (ctx.from.id == process.env.ADMIN_ID) {
-  //               AdminStart(ctx);
-  //             } else if (res?.admin == true) {
-  //               SubadminStart(ctx);
-  //             } else {
-  //               let custom = await db.getcustom();
-  //               ctx.reply(
-  //                 `<b>${
-  //                   custom?.welcome ? custom.welcome : customProps.welcome
-  //                 }</b>`,
-  //                 {
-  //                   parse_mode: "HTML",
-  //                 }
-  //               );
-  //             }
-  //           } else {
-  //             console.log("check user failed");
-  //           }
-  //         } else {
-  //           try {
-  //             const inlineKeyboard = new InlineKeyboard()
-  //               .url(
-  //                 "✔ JOIN CHANNEL",
-  //                 custom?.channelLink ?? process.env.INVITE_LINK
-  //               )
-  //               .row()
-  //               .text("♻️ Try again", "CHECK_CHANNEL");
-  //             ctx.reply(`🔖 <i>You must join our channel to use this bot</i>`, {
-  //               parse_mode: "HTML",
-  //               reply_markup: inlineKeyboard,
-  //             });
-  //           } catch (error) {
-  //             console.log(error);
-  //           }
-  //         }
-  //       });
-  //   } else {
-  //     ctx.reply(`You can no longer use this bot as you are banned`);
-  //   }
-  // });
-
-  console.log("startingss");
+      await ctx.api
+        .getChatMember(
+          custom?.channelID || process.env.MAIN_CHANNEL,
+          ctx.from.id
+        )
+        .then(async (status) => {
+          if (status.status != "left") {
+            if (query) {
+              handlequery(ctx);
+            } else if (res) {
+              if (ctx.from.id == process.env.ADMIN_ID) {
+                AdminStart(ctx);
+              } else if (res?.admin == true) {
+                SubadminStart(ctx);
+              } else {
+                let custom = await db.getcustom();
+                ctx.reply(
+                  `<b>${
+                    custom?.welcome ? custom.welcome : customProps.welcome
+                  }</b>`,
+                  {
+                    parse_mode: "HTML",
+                  }
+                );
+              }
+            } else {
+              console.log("check user failed");
+            }
+          } else {
+            try {
+              const inlineKeyboard = new InlineKeyboard()
+                .url(
+                  "✔ JOIN CHANNEL",
+                  custom?.channelLink ?? process.env.INVITE_LINK
+                )
+                .row()
+                .text("♻️ Try again", "CHECK_CHANNEL");
+              ctx.reply(`🔖 <i>You must join our channel to use this bot</i>`, {
+                parse_mode: "HTML",
+                reply_markup: inlineKeyboard,
+              });
+            } catch (error) {
+              console.log(error);
+            }
+          }
+        });
+    } else {
+      ctx.reply(`You can no longer use this bot as you are banned`);
+    }
+  });
 };
